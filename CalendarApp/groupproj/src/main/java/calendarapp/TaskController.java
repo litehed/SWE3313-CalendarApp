c2decd8b1319850762daa127429a7ce3a3061bca
Vanessa <nessaxrl@gmail.com>
2025-12-06 16:53:30 -0500

    Implemented task management methods in TaskController for calendarfx

--- CalendarApp/groupproj/src/main/java/calendarapp/TaskController.java
+++ CalendarApp/groupproj/src/main/java/calendarapp/TaskController.java
@@ -1,16 +1,32 @@
 package calendarapp;
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class TaskController
 {
-    private static List<Task> tasks;
+    private static List<Task> tasks = new ArrayList<>();
+    private static Map<String, Task> entryMap = new HashMap<>();
 
-    public static List<Task> getAllTasks() {
+    public static void addTask(Task t) {
+        tasks.add(t);
+        /* save (saveCalendarData() here?) */ }
+
+    public static void updateTask(Task t) {
+        /* save */ }
+
+    public static void deleteTask(Task t) {
+        tasks.remove(t);
+        /* save */ }
+    
+    public static List<Task> getTasks() {
         return tasks; }
 
+    public static void registerEntry(Task t, String entryId) {
+        entryMap.put(entryId, t); }
+
+    public static Task getTaskByEntryId(String id) {
+        return entryMap.get(id); }
+
 }
+
 
