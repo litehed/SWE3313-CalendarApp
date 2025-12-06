c5eb2a2053329eeea84175532eaca3bf3a816da5
Ethan Leitner <57595571+litehed@users.noreply.github.com>
2025-12-05 19:39:40 -0500

    Begun adding save tools

--- CalendarApp/groupproj/src/main/java/calendarapp/HomeController.java
+++ CalendarApp/groupproj/src/main/java/calendarapp/HomeController.java
@@ -5,7 +5,12 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class HomeController implements Initializable {
 
